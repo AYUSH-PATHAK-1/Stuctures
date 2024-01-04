b048cb95b549a86b9cdbc15ceb50fe1090e42755
AYUSH PATHAK <92669182+AYUSH-PATHAK-1@users.noreply.github.com>
2024-01-04 16:18:29 +0530

    Create sum of the array.cpp

---
+++ sum of the array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main(){
+    
+    int n=5;
+    std::vector<int>arr={1,2,3,4,5};
+    int sum=0;
+    for (int i=0;i<n;i++){
+    sum+=(double)arr[i];
+    }
+    std::cout << "the Sum is " << sum << std::endl;
+   
+}
